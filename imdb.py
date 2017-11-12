b97bce3951c4d9281769b6db9748651fdb0d0097
cakoch10 <cakoch10@gmail.com>
2017-11-12 04:11:45 -0500

    added artists

--- imdb.py
+++ imdb.py
@@ -2,13 +2,13 @@ import requests
 import json
 
 # put api key here
-apiKey = ""
+apiKey = "f3ba73ed65de9b13e01665d32af05100"
 
 # put imdb key here
-imdb_key = ""
+imdb_key = "318a3ddd"
 
 # put the music key here
-music_key = ""
+music_key = "c8303e90962e3a5ebd5a1f260a69b138"
 
 g_url = "https://api.themoviedb.org/3/genre/movie/list?api_key="+apiKey
 
@@ -16,6 +16,7 @@ genres_r = requests.get(g_url)
 genres = genres_r.content
 
 genres_json = json.loads(genres)["genres"]
+
 
 
 def get_data_genre(genre):
@@ -47,7 +48,6 @@ def get_data_title(title):
         return None
 
 
-
 def get_music_artist(artist):
     m_url = "http://api.musicgraph.com/api/v2/artist/search?api_key="+music_key
     nme = "&name="+artist
@@ -62,6 +62,14 @@ def get_music_artist_similar(artist):
     url_final = m_url+nme
     r = requests.get(url_final)
     return r.content
+
+def artst_sim_nms(artist):
+    r = get_music_artist_similar(artist)
+    js = json.loads(r)
+    names = []
+    for atst in js["data"]:
+        names.append(atst["name"])
+    return names
 
 def print_json(j):
     parse = json.loads(j)
